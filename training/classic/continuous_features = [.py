44d25681fe1545b8dc73262dc1c8960768fac1ed
Carlos <carlmycol@gmail.com>
2020-10-20 14:59:53 -0300

    Delete wrong file

--- training/classic/continuous_features = [.py
+++
@@ -1,7 +0,0 @@
-continuous_features = [
-    'Open', 'High', 'Low', 'Close', 'Volume',
-    'MACD', 'RSI', 'BB_High', 'BB_Mid', 'BB_Low',
-    'ATR', 'NATR', 'Currency_Volume', 'Adj Close'
-]
-categorical_features = ['Month', 'Weekday', 'Ticker', 'Currency']
-features = continuous_features + categorical_features
