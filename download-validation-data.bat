0dccded0c1ae150a43a18b8715c27e85e4eea022
Carlos <carlmycol@gmail.com>
2020-10-22 15:45:17 -0300

    Add results of training

---
+++ download-validation-data.bat
@@ -0,0 +1,4 @@
+(gsutil cp gs://algo-trading/return_signals/model_test_Forward_Return_3m.csv models/model_test_Forward_Return_3m.csv
+gsutil cp gs://algo-trading/return_signals/model_test_Forward_Return_2m.csv models/model_test_Forward_Return_2m.csv
+gsutil cp gs://algo-trading/return_signals/model_test_Forward_Return_1m.csv models/model_test_Forward_Return_1m.csv
+gsutil cp gs://algo-trading/return_signals/model_test_Forward_Return_1w.csv models/model_test_Forward_Return_1w.csv)
